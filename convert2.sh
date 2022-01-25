f52290574e4e44f9d10d20b5ebdb6503af7c394c
Gentleman-DE <70391383+Gentleman-DE@users.noreply.github.com>
2022-01-25 13:52:36 +0100

    Update convert2.sh

--- convert2.sh
+++ convert2.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 for x in *.mkv
-do numTracks=$(mkvinfo "$x"|grep -c "S_TEXT/ASS")
+do 
+	numTracks=$(mkvinfo "$x"|grep -c "S_TEXT/ASS")
 if [ $numTracks -ne 1 ]
 then
 	echo "No tracks to convert or more than one! Aborting."
 	exit 1
 fi
 for subTrack in $(mkvinfo "$x"|grep -B4 "S_TEXT/ASS"|grep "Track UID"|cut -d ":" -f 2)
-do	trackLang=$(mkvinfo "$x"|grep -B4 "S_TEXT/ASS"|grep -C3 "Track UID: $subTrack"|grep "Language:"|cut -d ":" -f 2|tr -d '\040\011\012\015')
+do
+	trackLang=$(mkvinfo "$x"|grep -B4 "S_TEXT/ASS"|grep -C3 "Track UID: $subTrack"|grep "Language:"|cut -d ":" -f 2|tr -d '\040\011\012\015')
 	if [ -z "$trackLang" ]
 	then
 		trackLang="eng"
@@ -25,12 +27,9 @@ do	trackLang=$(mkvinfo "$x"|grep -B4 "S_TEXT/ASS"|grep -C3 "Track UID: $subTrack
 		echo "Failed to merge subtitle to $x!"
 		exit 1
 	fi
-	rm out.srt
-	mkdir -p Converted
-	echo "Successfully converted $x to output.mkv with added SRT-subs!"
-	#rm $x
+	rm out.srt	
+	echo "Successfully converted $x to output.mkv with added SRT-subs!"	
 	mv output.mkv "$x"
-	
 	
 	break
 done
